caabeea04b5ea6194c5fce2394138f455c444763
min56 <minchaeg85@gmail.com>
2024-07-18 18:48:07 +0900

    create :: 프로젝트 생성

---
+++ src/test/kotlin/com/example/coblebackend/CobleBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.coblebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CobleBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
