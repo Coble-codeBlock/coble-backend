2a7655a588aaaa99ef59a88bd37dee3a962c4e11
min56 <minchaeg85@gmail.com>
2024-07-26 00:01:37 +0900

    update :: mail dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework:spring-context-support:5.3.9")
 }
 
 tasks.withType<KotlinCompile> {
