27d77b5cac1def0cc0cf27b4e02a4704c3980eee
min56 <minchaeg85@gmail.com>
2024-07-26 15:03:41 +0900

    update :: VERIFY_CODE_INVALID 추가

--- src/main/kotlin/com/example/coblebackend/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/coblebackend/global/error/exception/ErrorCode.kt
@@ -4,10 +4,12 @@ enum class ErrorCode(
     val status: Int,
     val message: String,
 ) {
+
     ALREADY_EMAIL_EXISTS(409, "Email already exists"),
 
     TOKEN_EXPIRED(401, "Token Expired"),
     TOKEN_INVALID(401, "Token Invalid"),
+    VERIFY_CODE_INVALID(401, "Verify Code Invalid"),
 
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 }
