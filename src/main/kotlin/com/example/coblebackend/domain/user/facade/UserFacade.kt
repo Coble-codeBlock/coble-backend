fa9fabc27bba7d14ce9ff1287d9b3b95918f761a
min56 <minchaeg85@gmail.com>
2024-07-29 15:09:00 +0900

    update :: getUserByEmail 메소드 추가

--- src/main/kotlin/com/example/coblebackend/domain/user/facade/UserFacade.kt
+++ src/main/kotlin/com/example/coblebackend/domain/user/facade/UserFacade.kt
@@ -1,6 +1,8 @@
 package com.example.coblebackend.domain.user.facade
 
+import com.example.coblebackend.domain.user.domain.User
 import com.example.coblebackend.domain.user.domain.repository.UserRepository
+import com.example.coblebackend.domain.user.exception.UserNotFoundException
 import org.springframework.stereotype.Component
 
 @Component
@@ -11,7 +13,10 @@ class UserFacade(
     fun checkUserExistsEmail(email: String): Boolean =
         userRepository.existsUserByEmail(email)
 
+    fun checkUserByNicknameOrEmail(nickname: String, email: String): Boolean =
+        userRepository.existsUserByNicknameOrEmail(nickname, email)
 
-    fun checkUserExistsNickname(nickname: String): Boolean =
-        userRepository.existsUserByNickname(nickname)
+    fun getUserByEmail(email: String): User {
+        return userRepository.findByEmail(email) ?: throw UserNotFoundException
+    }
 }
