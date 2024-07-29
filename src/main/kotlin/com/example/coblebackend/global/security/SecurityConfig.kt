91d60b6f0123c9fb9b8b73581734c39608c02ddc
min56 <minchaeg85@gmail.com>
2024-07-29 15:09:38 +0900

    update :: securityFilterChain 설정 추가

--- src/main/kotlin/com/example/coblebackend/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/coblebackend/global/security/SecurityConfig.kt
@@ -1,14 +1,44 @@
 package com.example.coblebackend.global.security
 
+import com.example.coblebackend.global.config.FilterConfig
+import com.example.coblebackend.global.security.jwt.JwtTokenProvider
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @EnableWebSecurity
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    private val objectMapper: ObjectMapper,
+    private val jwtTokenProvider: JwtTokenProvider
+) {
+
+    @Bean
+    @Throws(Exception::class)
+    protected fun securityFilterChain(http: HttpSecurity): SecurityFilterChain? {
+        return http
+            .csrf().disable()
+            .formLogin().disable()
+            .cors()
+
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeHttpRequests()
+            .antMatchers("/**").permitAll()
+            .anyRequest().permitAll()
+
+            .and().apply(FilterConfig(objectMapper, jwtTokenProvider))
+            .and().build()
+    }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
