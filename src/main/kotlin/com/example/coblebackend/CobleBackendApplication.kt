ebbaa4cae10eced0663dd190a0bf6e327124756c
min56 <minchaeg85@gmail.com>
2024-08-05 23:37:14 +0900

    update :: Properties 추가

--- src/main/kotlin/com/example/coblebackend/CobleBackendApplication.kt
+++ src/main/kotlin/com/example/coblebackend/CobleBackendApplication.kt
@@ -1,11 +1,15 @@
 package com.example.coblebackend
 
+import com.example.coblebackend.global.config.s3.S3Properties
 import com.example.coblebackend.global.security.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@EnableConfigurationProperties(JwtProperties::class)
+@EnableConfigurationProperties(
+	JwtProperties::class,
+	S3Properties::class,
+)
 @SpringBootApplication
 class CobleBackendApplication
 
