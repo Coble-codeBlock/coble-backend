d56595920ea8e5b02535ff5788e9c384071dcebd
min56 <minchaeg85@gmail.com>
2024-08-08 22:56:39 +0900

    update :: columnDefinition 추가

--- src/main/kotlin/com/example/coblebackend/domain/user/domain/User.kt
+++ src/main/kotlin/com/example/coblebackend/domain/user/domain/User.kt
@@ -14,7 +14,7 @@ import javax.validation.constraints.Size
 class User (
     @field:NotNull
     @field:Length(min = 2, max = 20)
-    @Column(nullable = false, unique = true)
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false, unique = true)
     val nickname: String,
 
     @field:NotNull
