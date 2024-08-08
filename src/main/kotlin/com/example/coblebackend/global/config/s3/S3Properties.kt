ba20c689cb4e6013d1e2ef062f2705a6a7878f39
min56 <minchaeg85@gmail.com>
2024-08-08 22:57:04 +0900

    update :: @ConstructorBinding 추가

--- src/main/kotlin/com/example/coblebackend/global/config/s3/S3Properties.kt
+++ src/main/kotlin/com/example/coblebackend/global/config/s3/S3Properties.kt
@@ -1,12 +1,14 @@
 package com.example.coblebackend.global.config.s3
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
+@ConstructorBinding
 @ConfigurationProperties(prefix = "cloud.aws")
 data class S3Properties(
     val accessKey: String,
     val secretKey: String,
-    val bucket: String?,
-    val url: String?,
-    val static: String?,
+    val bucket: String,
+    val url: String,
 )
+
