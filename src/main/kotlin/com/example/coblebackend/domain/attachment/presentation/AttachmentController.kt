a0934ffb93ef1263e74ae55c6c01afcbf0bcbdd7
min56 <minchaeg85@gmail.com>
2024-08-08 22:55:32 +0900

    update :: ResponseStatus 추가

--- src/main/kotlin/com/example/coblebackend/domain/attachment/presentation/AttachmentController.kt
+++ src/main/kotlin/com/example/coblebackend/domain/attachment/presentation/AttachmentController.kt
@@ -2,8 +2,10 @@ package com.example.coblebackend.domain.attachment.presentation
 
 import com.example.coblebackend.domain.attachment.presentation.dto.response.UploadAttachmentResponse
 import com.example.coblebackend.domain.attachment.service.AttachmentUploadService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
@@ -13,6 +15,7 @@ class AttachmentController(
     private val attachmentUploadService: AttachmentUploadService,
 ) {
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/upload")
     fun uploadAttachment(attachment: MultipartFile): UploadAttachmentResponse {
         return attachmentUploadService.execute(attachment)
