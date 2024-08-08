dc6bdb111f475527015576341c2838728f97d3f8
min56 <minchaeg85@gmail.com>
2024-08-08 22:55:46 +0900

    update :: @Column 추가

--- src/main/kotlin/com/example/coblebackend/domain/project/domain/Project.kt
+++ src/main/kotlin/com/example/coblebackend/domain/project/domain/Project.kt
@@ -2,6 +2,7 @@ package com.example.coblebackend.domain.project.domain
 
 import com.example.coblebackend.domain.user.domain.User
 import com.example.coblebackend.global.entity.BaseIdEntity
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType
 import javax.persistence.JoinColumn
@@ -14,8 +15,10 @@ class Project(
 
     val image: String,
 
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
     val title: String,
 
+    @Column(columnDefinition = "VARCHAR(100)")
     val description: String,
 
     val codeFile: String,
