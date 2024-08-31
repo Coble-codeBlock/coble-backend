c4adba8897b6a99f5a94bc2d3630415b10bfcf69
min56 <minchaeg85@gmail.com>
2024-09-01 02:33:12 +0900

    create :: 문제 해결 API

--- src/main/kotlin/com/example/coblebackend/domain/quiz/presentation/QuizController.kt
+++ src/main/kotlin/com/example/coblebackend/domain/quiz/presentation/QuizController.kt
@@ -5,11 +5,15 @@ import com.example.coblebackend.domain.quiz.presentation.dto.response.GetQuizAns
 import com.example.coblebackend.domain.quiz.presentation.dto.response.GetQuizListResponse
 import com.example.coblebackend.domain.quiz.service.GetQuizAnswerListService
 import com.example.coblebackend.domain.quiz.service.GetQuizListService
+import com.example.coblebackend.domain.quiz.service.QuizSolveService
 import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController
 class QuizController(
     private val getQuizListService: GetQuizListService,
     private val getQuizAnswerListService: GetQuizAnswerListService,
+    private val quizSolveService: QuizSolveService,
 ) {
 
     @GetMapping("/list")
@@ -33,5 +38,13 @@ class QuizController(
         @PathVariable(name = "quiz-title-id") quizTitleId: Long,
     ): GetQuizAnswerListResponse {
         return getQuizAnswerListService.getQuizAnswerList(quizTitleId)
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/solve/{quiz-title-id}")
+    fun quizSolve(
+        @PathVariable(name = "quiz-title-id") quizTitleId: Long,
+    ) {
+        quizSolveService.quizSolve(quizTitleId)
     }
 }
---
+++ src/main/kotlin/com/example/coblebackend/domain/quiz/service/QuizSolveService.kt
@@ -0,0 +1,29 @@
+package com.example.coblebackend.domain.quiz.service
+
+import com.example.coblebackend.domain.quiz.domain.QQuizTitle.quizTitle
+import com.example.coblebackend.domain.quiz.domain.UserAnswer
+import com.example.coblebackend.domain.quiz.domain.repository.UserAnswerRepository
+import com.example.coblebackend.domain.quiz.facade.QuizTitleFacade
+import com.example.coblebackend.domain.user.facade.UserFacade
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class QuizSolveService(
+    private val userFacade: UserFacade,
+    private val quizTitleFacade: QuizTitleFacade,
+    private val userAnswerRepository: UserAnswerRepository,
+) {
+
+    @Transactional
+    fun quizSolve(quizTitleId: Long) {
+        val quizTitle = quizTitleFacade.getQuizTitleByQuizId(quizTitleId)
+        val user = userFacade.getCurrentUser()
+
+        userAnswerRepository.save(UserAnswer(
+            quizTitle = quizTitle,
+            user = user,
+            quizStatus = true
+        ))
+    }
+}
