c1fcb9feba0e297095cbb5a7e549724dc8315e73
min56 <minchaeg85@gmail.com>
2024-09-01 18:04:20 +0900

    fix :: url 반환 수정

--- src/main/kotlin/com/example/coblebackend/domain/project/service/GetProjectInfoService.kt
+++ src/main/kotlin/com/example/coblebackend/domain/project/service/GetProjectInfoService.kt
@@ -4,6 +4,7 @@ import com.example.coblebackend.domain.project.exception.NotWriterUserException
 import com.example.coblebackend.domain.project.facade.ProjectFacade
 import com.example.coblebackend.domain.project.presentation.dto.response.GetProjectInfoResponse
 import com.example.coblebackend.domain.user.facade.UserFacade
+import com.example.coblebackend.global.utils.S3Util
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional
 class GetProjectInfoService(
     private val projectFacade: ProjectFacade,
     private val userFacade: UserFacade,
+    private val s3Util: S3Util,
 ) {
 
     @Transactional(readOnly = true)
@@ -23,9 +25,11 @@ class GetProjectInfoService(
             requestUserId = user.id,
         )
 
+        val imageUrl = s3Util.getS3ObjectUrl(project.image)
+
         return GetProjectInfoResponse(
             id = project.id,
-            image = project.image,
+            image = imageUrl,
             title = project.title,
             description = project.description,
         )
