dd057e5ee5f3ff6e0257b4e470ede15a798e8826
min56 <minchaeg85@gmail.com>
2024-09-08 13:18:03 +0900

    fix :: delete 제거

--- src/main/kotlin/com/example/coblebackend/domain/project/service/SaveProjectCodeFileService.kt
+++ src/main/kotlin/com/example/coblebackend/domain/project/service/SaveProjectCodeFileService.kt
@@ -1,7 +1,6 @@
 package com.example.coblebackend.domain.project.service
 
 import com.example.coblebackend.domain.project.facade.ProjectFacade
-import com.example.coblebackend.domain.project.presentation.dto.request.SaveProjectCodeFileRequest
 import com.example.coblebackend.domain.user.facade.UserFacade
 import com.example.coblebackend.global.utils.S3Util
 import org.springframework.stereotype.Service
@@ -19,16 +18,11 @@ class SaveProjectCodeFileService(
     fun execute(projectId: Long, codeFile: MultipartFile) {
         val user = userFacade.getCurrentUser()
         val project = projectFacade.getProjectById(projectId)
-        val checkCodeFileExist = project.codeFile != null
 
         projectFacade.checkProjectCreateUser(
             createUserId = project.user.id,
             requestUserId = user.id
         )
-
-        if(checkCodeFileExist) {
-            s3Util.delete(project.codeFile, "")
-        }
 
         val codeFileUrl = s3Util.uploadImage(codeFile)
 
